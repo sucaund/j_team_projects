e06d8a65693f0125268b1c775b3eb6210bd677eb
jaman <ujm1jaman@gmail.com>
2024-02-22 11:51:53 +0900

    jpa수정3

--- hellong/src/main/java/com/oracle/hellong/controller/JMController.java
+++ hellong/src/main/java/com/oracle/hellong/controller/JMController.java
@@ -15,7 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 public class JMController {
 	private final JMService jm;
 	
-	private final JavaMailSender mailSender; // 이것또한 생성자 생성됨
+	private final JavaMailSender mailSender; // 이것또한 생성자 생성됨 3
 
 
 
