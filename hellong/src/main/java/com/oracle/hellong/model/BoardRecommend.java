a28a46d8620452b51e7b3a3042fd5777ccaf7e2e
prog91 <prog91@naver.com>
2024-03-16 14:42:21 +0900

    3 / 16 commit!

---
+++ hellong/src/main/java/com/oracle/hellong/model/BoardRecommend.java
@@ -0,0 +1,12 @@
+package com.oracle.hellong.model;
+
+import lombok.Data;
+
+@Data
+public class BoardRecommend {
+	private int recommend_id;
+	private int m_number;
+	private int b_number;
+	private int isrecommended;
+	
+}
